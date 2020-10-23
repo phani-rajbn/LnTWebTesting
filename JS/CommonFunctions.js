bcf6ec5b0163b130cc66d59e030549da091198c8
Phaniraj B.N <35284581+phani-rajbn@users.noreply.github.com>
2020-10-23 09:57:00 +0530

    Add files via upload

--- JS/CommonFunctions.js
+++ JS/CommonFunctions.js
@@ -26,6 +26,13 @@ const mulFunc = function(v1, v2){
 const divFunc = function(v1, v2){
     return v1 / v2;
 }
+const getDate = function(dt){
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const year = dt.getFullYear();
+    const month = months[dt.getMonth()];
+    const day = dt.getDate();
+    return day + " " + month + " " + year;
+}
 
 
 
