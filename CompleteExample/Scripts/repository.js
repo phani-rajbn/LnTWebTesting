db31cacbb74e6bf9fa242ec9e476db361941a992
Phaniraj B.N <35284581+phani-rajbn@users.noreply.github.com>
2020-10-24 12:33:53 +0530

    Add files via upload

--- CompleteExample/Scripts/repository.js
+++ CompleteExample/Scripts/repository.js
@@ -1,9 +1,10 @@
 class employee{
-    constructor(id, name, email, salary) {
+    constructor(id, name, email, salary, pic) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.salary = salary;
+        this.pic = pic;
     }
 }
 
@@ -26,12 +27,32 @@ class empRepository{
            const url = "http://localhost:3000/employees";
            //get is async, so create a Promise Object and in the callback of the data, U return the success(data) with data of the get as arg(Return value)
            $.get(url, (data) => {
-               debugger;
-               alert("All employees found")
                this.employees = data;
                success(this.employees);
            }); 
        });
        return promise;  
+    }
+
+    getEmployee(id){
+        const promise = new Promise((res, err)=>{
+            const url = "http://localhost:3000/employees/" + id;
+            $.get(url, (data)=>{
+                res(data);
+            })
+        })
+        return promise;
+    }
+
+    updateEmployee(emp){
+        const url = "http://localhost:3000/employees/" + emp.id;
+        $.ajax({
+            url: url,
+            data : emp,
+            method:"PUT",
+            success: (data)=>{
+                alert("Employee updated successfully")
+            }
+        })
     }
 }
